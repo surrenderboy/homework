179cfa8eb265b0ee95136e97d70117348befe759
Ilya Demukh <demukh.ilya@gmail.com>
2018-03-28 13:53:11 +0300

    landscape support for gallery view

--- src/Gallery.js
+++ src/Gallery.js
@@ -20,13 +20,15 @@ class Gallery extends React.Component {
 
     return (
       <div className="gallery">
-        <div className="gallery__control gallery__control_prev" onClick={handlePrev}>
-          { hasPrev() && <div className="gallery__prev">&lt;</div> }
+        <div className="gallery__nav gallery__nav_prev" onClick={handlePrev}>
+          { hasPrev() && <div className="gallery__icon gallery__icon_prev">&lt;</div> }
         </div>
-        <div className="gallery__close" onClick={handleClose}>X</div>
+        <div className="gallery__nav gallery__nav_close" onClick={handleClose}>
+          <div className="gallery__icon gallery__icon_close">X</div>
+        </div>
         <img src={item.img['L'].href} alt={item.title} className={'gallery__image'} onClick={handleNext}/>
-        <div className="gallery__control gallery__control_next" onClick={handleNext}>
-          { hasNext() && <div className="gallery__next">&gt;</div> }
+        <div className="gallery__nav gallery__nav_next" onClick={handleNext}>
+          { hasNext() && <div className="gallery__icon gallery__icon_next">&gt;</div> }
         </div>
       </div>
     );
