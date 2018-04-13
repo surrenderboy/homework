c7edb8d92448c88dec6cfcd8950639becf1293f1
Ilya Demukh <demukh.ilya@gmail.com>
2018-04-14 02:58:16 +0300

    added Root container

--- src/components/App.js
+++ src/components/App.js
@@ -1,108 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-import Collection from './Collection.js';
-import Gallery from "./Gallery";
-import PreviewImage from "./PreviewImage";
-import Spinner from "./Spinner";
-import unsplash from "../unsplash";
-import {toJson} from "unsplash-js";
+import PreviewPhotosCollection from "../containers/PreviewPhotosCollection";
+import PhotoGallery from "../containers/PhotoGallery";
 
-class App extends Component {
-  state = {
-    isLoading: true,
-    selectedItemIndex: -1,
-    items: []
-  };
+function App({ showGallery }) {
+  return(
+    <div className="app">
+      <PreviewPhotosCollection />
 
-  constructor(props) {
-    super(props);
-
-    this.hasNext = this.hasNext.bind(this);
-    this.hasPrev = this.hasPrev.bind(this);
-    this.openGallery = this.openGallery.bind(this);
-    this.closePreview = this.closePreview.bind(this);
-    this.showNext = this.showNext.bind(this);
-    this.showPrev = this.showPrev.bind(this);
-  }
-
-
-  componentDidMount() {
-    unsplash.photos.listPhotos(1, 12)
-      .then(toJson)
-      .then(items =>
-        this.setState({
-          isLoading: false,
-          items,
-        })
-      );
-  }
-
-  hasNext() {
-    return this.state.selectedItemIndex + 1 < this.state.items.length;
-  }
-
-  hasPrev() {
-    return this.state.selectedItemIndex > 0;
-  }
-
-  openGallery(item) {
-    this.setState(prevState => (
-      { selectedItemIndex: prevState.items.indexOf(item) }
-    ));
-  }
-
-  closePreview() {
-    this.setState({
-      selectedItemIndex: -1,
-    });
-  }
-
-  showNext() {
-    if (!this.hasNext()) return;
-
-    this.setState(prevState => (
-      { selectedItemIndex: prevState.selectedItemIndex + 1 }
-    ));
-  }
-
-  showPrev() {
-    if (!this.hasPrev()) return;
-
-    this.setState(prevState => (
-      { selectedItemIndex: prevState.selectedItemIndex - 1 }
-    ));
-  }
-
-  render() {
-    const previewIsOpen = this.state.selectedItemIndex >= 0;
-
-    return(
-      <div className="app">
-        {this.state.isLoading && <Spinner className="app__spinner" />}
-
-        <Collection>
-          {
-            this.state.items.map(item =>
-              <PreviewImage key={item.id} image={item} handleClick={this.openGallery}/>
-            )
-          }
-        </Collection>
-
-        {
-          previewIsOpen &&
-            <Gallery
-              item={this.state.items[this.state.selectedItemIndex]}
-              hasNext={this.hasNext}
-              hasPrev={this.hasPrev}
-              handleClose={this.closePreview}
-              handleNext={this.showNext}
-              handlePrev={this.showPrev}
-            />
-        }
-      </div>
-    );
-  }
+      { showGallery && <PhotoGallery /> }
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {applyMiddleware, createStore} from "redux";
+import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+
 import './index.css';
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './reducers';
+import Root from "./containers/Root";
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Root />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
